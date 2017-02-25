ba3068d639b8cdb5a78cdb5bb742459bd193ce5a
veganet <veganet>
2017-02-25 11:01:17 -0600

    Initial commit

---
+++ app/src/main/java/mx/udg/valles/cursoandroidvalles/Splash.java
@@ -0,0 +1,40 @@
+package mx.udg.valles.cursoandroidvalles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by abraham on 18/02/2017.
+ */
+
+public class Splash extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread()
+        {
+
+            @Override
+            public void run()
+            {
+                super.run();
+                try {
+                    sleep(3500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                       Intent intent = new Intent(Splash.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+            }
+        }
+    };
+        thread.start();
+  }
+}
